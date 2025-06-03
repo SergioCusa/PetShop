82786fe61b72e103d8e27581ab3b8b9bf4e20a36
Leanpe <leanperez2112@gmail.com>
2025-06-03 17:17:26 -0300

    AccountScreen

--- app/src/main/java/com/prueba/petsop/ui/components/buttons/PrimaryButton.kt
+++ app/src/main/java/com/prueba/petsop/ui/components/buttons/PrimaryButton.kt
@@ -1,6 +1,5 @@
 package com.prueba.petsop.ui.components.buttons
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,7 +9,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
